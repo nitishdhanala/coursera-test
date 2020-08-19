04afb56bc6f0f8cca1a6a4704c5ecf1363923508
nitishdhanala <51997053+nitishdhanala@users.noreply.github.com>
2020-08-19 17:57:26 +0530

    Update ajax-utils.js

--- module5-solution/js/ajax-utils.js
+++ module5-solution/js/ajax-utils.js
@@ -6,10 +6,10 @@ var ajaxUtils = {};
 
 // Returns an HTTP request object
 function getRequestObject() {
-  if (window.XMLHttpRequest) {
+  if (global.XMLHttpRequest) {
     return (new XMLHttpRequest());
   } 
-  else if (window.ActiveXObject) {
+  else if (global.ActiveXObject) {
     // For very old IE browsers (optional)
     return (new ActiveXObject("Microsoft.XMLHTTP"));
   } 
@@ -64,4 +64,3 @@ global.$ajaxUtils = ajaxUtils;
 
 
 })(window);
-
